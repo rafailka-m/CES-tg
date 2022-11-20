10be5185cd681a9f51d8fac18f57b30ed10a68cb
gest0rbn <rafgest0rbn@mail.ru>
2022-11-20 04:15:44 +0300

    updated

--- telegram/middlewares/access.py
+++ telegram/middlewares/access.py
@@ -6,28 +6,28 @@ from emoji import emojize
 from settings.config import STAFF
 
 
-SWEDEN_FLAG = emojize(':flag_se:')
-UK_FLAG = emojize(':gb:')
+SWEDEN_FLAG = emojize(':sweden:')
+UK_FLAG = emojize(':great_britain:')
 
 
 class AccessMiddleware(BaseMiddleware):
 
-    async def pre_process_message(self,
-                                  message: types.Message,
-                                  dict):
+    async def on_pre_process_message(self,
+                                     message: types.Message,
+                                     data: dict):
         if message.from_user.id not in STAFF:
             await message.answer(
-                f'{SWEDEN_FLAG} <b>Ingen åtkomst!</b>'
+                f'{SWEDEN_FLAG} <b>Ingen åtkomst!</b>\n'
                 f'{UK_FLAG} <b>No access!</b>'
             )
             raise CancelHandler()
 
-    async def pre_process_callback_query(self,
-                                  callback: types.CallbackQuery,
-                                  dict):
+    async def on_pre_process_callback_query(self,
+                                            callback: types.CallbackQuery,
+                                            data: dict):
         if callback.from_user.id not in STAFF:
             await callback.message.answer(
-                f'{SWEDEN_FLAG} <b>Ingen åtkomst!</b>'
+                f'{SWEDEN_FLAG} <b>Ingen åtkomst!</b>\n'
                 f'{UK_FLAG} <b>No access!</b>'
             )
             await callback.answer()
